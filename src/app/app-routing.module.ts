c5eb799bf17c0d7b465a9a91b91a4c3d805fa8b3
LaiaRodriguez22 <lrodrigu22@jaumebalmes.net>
2023-12-11 09:34:37 +0100

    Luxon installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelpComponent } from './pages/help/help.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: HelpComponent },
+  {
+    path: 'forgotPassword',
+    loadChildren: () => import('./pages/help/help.module').then(m => m.HelpModule)
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
